99c67e512bf96160586f166b9fb42e70469dee2c
Maurício Taborda <mauriciotabordamts@gmail.com>
2026-06-01 10:04:33 -0300

    data(auto): atualizacao automatica de dados do fluxo de caixa

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 ﻿window.CASH_FLOW_DATA = {
-    "generated_at":  "2026-06-01 09:03:13",
+    "generated_at":  "2026-06-01 10:04:30",
     "categories_origin":  {
                               "entradas":  [
 
