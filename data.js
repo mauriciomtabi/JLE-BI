4f4b34e428c0ea853b62e0cf52b306bb9c5fd71f
Maurício Taborda <mauriciotabordamts@gmail.com>
2026-06-03 17:04:11 -0300

    data(auto): atualizacao automatica de dados e cache do PWA

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 ﻿window.CASH_FLOW_DATA = {
-    "generated_at":  "2026-06-03 16:17:28",
+    "generated_at":  "2026-06-03 17:04:09",
     "categories_origin":  {
                               "entradas":  [
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jle-bi-v3.12.20260603161729';
+const CACHE_NAME = 'jle-bi-v3.12.20260603170411';
 const ASSETS = [
   './',
   './index.html',
