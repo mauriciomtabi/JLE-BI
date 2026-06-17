d5bbc9920f6acb926f72c8d5e3f3f60ca0b9916b
Maurício Taborda <mauriciotabordamts@gmail.com>
2026-06-17 16:03:25 -0300

    data(auto): atualizacao automatica de dados e cache do PWA

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 ﻿window.CASH_FLOW_DATA = {
-    "generated_at":  "2026-06-17 15:03:57",
+    "generated_at":  "2026-06-17 16:03:23",
     "categories_origin":  {
                               "entradas":  [
 
