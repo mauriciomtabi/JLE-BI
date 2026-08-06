2604d704c1f6c19fb4b11d86c6152a380eb81bf2
Maurício Taborda <mauriciotabordamts@gmail.com>
2026-08-06 16:00:19 -0300

    data(manutencao): atualizacao automatica da base de Manutencao via Google Sheets

--- manutencao_data.js
+++ manutencao_data.js
@@ -1,7 +1,7 @@
 // Data generated 100% from Master Excel Controle de Medições (Mnt. Demanda)
 (function() {
     const db = {
-  "generated_at": "2026-08-06 14:00:15",
+  "generated_at": "2026-08-06 16:00:17",
   "lookups": {
     "tipos_of": [
       "TIPO\nOS CLIENTE",
