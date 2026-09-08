2866fa26e9189ffd812834091bad8861dbf00f5c
Maurício Taborda <mauriciotabordamts@gmail.com>
2026-09-08 18:00:23 -0300

    data(sar): atualizacao automatica da base SAR via rede local

--- manutencao_data.js
+++ manutencao_data.js
@@ -1,7 +1,7 @@
 // Data generated 100% from Master Excel Controle de Medições (Mnt. Demanda)
 (function() {
     const db = {
-  "generated_at": "2026-09-08 17:01:26",
+  "generated_at": "2026-09-08 18:00:21",
   "lookups": {
     "tipos_of": [
       "TIPO\nOS CLIENTE",
