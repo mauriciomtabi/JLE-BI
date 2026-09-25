261ddb540a1167cd20854895e8eeb2566c94bc4b
Maurício Taborda <mauriciotabordamts@gmail.com>
2026-09-25 12:00:31 -0300

    data(mdu): atualizacao automatica da base MDU via Google Sheets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker de Autodestruição
 // Remove a si mesmo e limpa todos os caches para resolver problemas de cache persistente no BI.
-// Versao: 2026-09-25 11:00:49
+// Versao: 2026-09-25 12:00:30
 
 self.addEventListener('install', event => {
   self.skipWaiting();
